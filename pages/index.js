a7c15369855dbf091f1f626d375d219be39e1eb6
dexterR35 <pusaw202@gmail.com>
2023-11-23 16:48:40 +0200

    update

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,17 @@ import Services from "../components/Services";
 import Hero from "../components/Hero";
 import Layout from "../components/Layout/Layout";
 import SeoHead from "../components/SeoHead";
-
+import MainForm from "../components/MainForm";
 export default function Home() {
   return (
     <>
-      <SeoHead title="Obtine credit" />
+      {/* <SeoHead title="Obtine credit" />
       <Layout>
         <Hero />
         <Feature />
         <Services />
-      </Layout>
+      </Layout> */}
+      <MainForm />
     </>
   );
 }
